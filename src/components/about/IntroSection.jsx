136e6f4ffe005823323edac603eec48dc640898f
Nitin6404 <nc1575612@gmail.com>
2025-03-14 13:25:56 +0530

    fix: about us spacing

--- src/components/about/IntroSection.jsx
+++ src/components/about/IntroSection.jsx
@@ -1,13 +1,13 @@
 // Introduction section component
 export function IntroSection() {
   return (
-    <div className="mt-[80px] flex w-full max-w-[1300px] flex-col items-center justify-center px-4 text-center">
+    <div className="mt-[70px] flex w-full max-w-[1300px] flex-col items-center justify-center px-4 text-center">
       <p className="w-full text-start font-inter text-lg font-normal leading-[22px] text-[#434343]">
         For the past 15 years, we&apos;ve been dedicated to helping thousands of individuals repair
         their credit and reclaim financial control. At EzeCredit, we fight for every consumer&apos;s
         right to an accurate, fair, and substantiated credit report.
       </p>
-      <h2 className="mt-[50px] font-inter text-[50px] font-bold leading-[60px] text-[#15549A]">
+      <h2 className="mt-[48px] font-inter text-[50px] font-bold leading-[60px] text-[#15549A]">
         Our Program
       </h2>
       <p className="mt-2 font-inter text-[29px] font-bold leading-[35px] text-[#4B4B4B]">
